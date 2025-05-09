b023c8581f698e0825293675438a80e8701723c9
cedric-castelino <160269453+cedric-castelino@users.noreply.github.com>
2025-05-10 00:20:37 +1000

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
             <p className="text-md mt-5 w-[95%] text-center">I am a conscientious, motivated, and hard-working tertiary student dedicated to high-quality work and maximising outcomes. I have garnered a range of experience with working individually and collaboratively in group environments. Additionally, I have a strong technological background, stemming from educational and personal experience, integrated with a commitment to providing timely and quality customer service.</p>
             <p className="text-md mt-5 mb-5 w-[95%] text-center">Currently, I am pursuing a double degree at UNSW, working towards a Bachelor degree in Computer Science and Commerce. I hope to bridge my passion and expertise for both of these fields, in order to demonstrate a holistically capable working ability. I have also completed multiple certificates during my time in secondary school, aquiring a Certificate III in Information, Digital Media and Technology and a Certificate II in Active Volunteering. These certifications are rigorously tested and nationally recognised indicators of my abilities.</p>
             </div>
-            <div className="bg-base-200 flex flex-col items-center w-[94%] mb-5 rounded-xl">
+            <div className="bg-base-200 flex flex-col items-center w-[93%] mb-5 rounded-xl">
             <h1 className="text-2xl font-bold ml-4 mt-5">Skills</h1>
             <div className="flex flex-wrap justify-center gap-4 mt-5 mb-10 pl-4">
               <div className="card bg-neutral w-60 shadow-sm">
